b2fb2aa8832f8e96d0d994e35f2b0a8cf54c9572
Manish Chaudhary <manish.chaudhary@kelltontech.com>
2017-05-02 14:13:06 +0530

    problem 4

--- wordfreq.py
+++ wordfreq.py
@@ -1,11 +1,16 @@
 """open a text file, read the text, process it using clearnwords. Count the number of occurences of each unique word"""
 from clearnwords import cleanwords
+noisewords = []
+with open('./noisewords', 'r') as n:
+	for line in n:
+		noisewords += line.split(',')		
 counts = {}
 with open('./assignment', 'r') as f:
 	for line in f:
 		for word in cleanwords(line):
-			if word in counts:
-				counts[word] += 1
-			else:
-				counts[word] = 1
+			if word not in noisewords:				
+				if word in counts:
+					counts[word] += 1
+				else:
+					counts[word] = 1
 	print counts
